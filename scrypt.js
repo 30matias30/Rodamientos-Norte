3b4f06ae2ed2d45e48bec964213d78dadbd6f997
Matias Astorga <30matias30@gmail.com>
2023-07-13 23:33:53 -0300

    Loading Screen - Fourth Draft

--- scrypt.js
+++ scrypt.js
@@ -3,3 +3,8 @@ const loader = document.getElementById('preloader');
 window.addEventListener("load", function() {
     loader.classList.toggle ("inActive");
 });
+
+loader.onclick = function() {
+
+    loader.style.display="none";
+}
