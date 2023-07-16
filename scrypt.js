98e26901ac56d2ed16de4122efb106d9ad957b5f
Matias Astorga <30matias30@gmail.com>
2023-07-16 20:57:18 -0300

    Input Slider - Minor fixes (loading screen & h2 padding)

--- scrypt.js
+++ scrypt.js
@@ -1,10 +1,21 @@
 const loader = document.getElementById('preloader');
+const loaderLogo = document.getElementById('preloaderLogo');
 
 window.addEventListener("load", function() {
     loader.classList.toggle ("inActive");
+    loaderLogo.classList.toggle("inActive");
 });
 
 loader.onclick = function() {
 
     loader.style.display="none";
+    loaderLogo.style.display="none";
+
+}
+
+loaderLogo.onclick = function() {
+
+    loader.style.display="none";
+    loaderLogo.style.display="none";
+
 }
