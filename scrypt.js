c4db5460a65e21be2f97e69c00cefd70c125ebae
Matias Astorga <30matias30@gmail.com>
2023-08-15 11:57:50 -0300

    First Client Test

--- scrypt.js
+++ scrypt.js
@@ -22,12 +22,38 @@ loaderLogo.onclick = function() {
 
 }
 
-const ini = document.getElementById('ini');
-const head = document.getElementById('header');
+// const ini = document.getElementById('ini');
+// const head = document.getElementById('header');
 
-ini.addEventListener('click', () => {
+// ini.addEventListener('click', () => {
 
-    head.classList.remove('header');
-    head.classList.add('secondHeader');
+//     head.classList.remove('header');
+//     head.classList.add('secondHeader');
+// })
+
+const optionButton = document.querySelectorAll('.option');
+
+optionButton.forEach( div => {
+    div.addEventListener("click", function() {
+        const url = div.getAttribute("data-url");
+        switch (url) {
+            case "./index.html":
+                window.location.href = url;
+                break;
+            case "./productos.html":
+                window.location.href = url;
+                break
+            case "./institucional.html":
+                window.location.href = url;
+                break;
+            case "./catalogos.html":
+                window.location.href = url;
+                break;
+            case "./contacto.html":
+                window.location.href = url;
+                break;
+            default:
+                window.alert("URL no válida");
+        }
+    })
 })
-
