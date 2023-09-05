bf0e1114d05164b405c47b59e02f241eca850bf9
Matias Astorga <30matias30@gmail.com>
2023-09-05 18:15:45 -0300

    Mobile Version - Contact Form First Draft

--- scrypt.js
+++ scrypt.js
@@ -134,6 +134,27 @@ footerButton.forEach(div => {
     });
 })
 
+
+// CONTACTO FORM MOBILE VERSION
+
+const formCard1 = document.querySelector('#contact-form-card1');
+const formCard2 = document.querySelector('#contact-form-card2');
+const formCard3 = document.querySelector('#contact-form-card-invisible');
+
+const nextButton1 = document.querySelector('.nextButton1');
+const nextButton2 = document.querySelector('.nextButton2');
+
+nextButton1.addEventListener('click', () => {
+    formCard1.classList.toggle('contact-form-card-invisible');
+    formCard2.classList.toggle('contact-form-card-invisible');
+})
+
+nextButton2.addEventListener('click', () => {
+    formCard2.classList.toggle('contact-form-card-invisible');
+    formCard3.id = '';
+})
+
+
 // MOBILE SLIDER
 
 const carousel = document.querySelector(".carousel"),
@@ -216,3 +237,5 @@ carousel.addEventListener("touchmove", dragging);
 
 document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("touchend", dragStop);
+
+
