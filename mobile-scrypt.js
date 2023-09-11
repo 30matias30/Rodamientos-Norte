84cf11278ae9724271b9d71e40910e9abf8cf1f5
Matias Astorga <30matias30@gmail.com>
2023-09-11 15:01:11 -0300

    Mobile - News Page - Third Draft

--- mobile-scrypt.js
+++ mobile-scrypt.js
@@ -4,42 +4,48 @@ mobileSlide.forEach( m => {
     m.addEventListener('click', function() {
         const url = m.getAttribute('data-url');
         switch(url) {
-            case 'Noticial/MobileNews-2.html':
+            case 'Noticias/MobileNews-1.html':
                 window.location.href = url;
                 break;
 
-            case 'Noticial/MobileNews-2.html':
+            case 'Noticias/MobileNews-2.html':
             window.location.href = url;
             break;
             
-            case 'Noticial/MobileNews-3.html':
+            case 'Noticias/MobileNews-3.html':
                 window.location.href = url;
                 break;
 
-            case 'Noticial/MobileNews-4.html':
+            case 'Noticias/MobileNews-4.html':
             window.location.href = url;
             break;
 
-            case 'Noticial/MobileNews-5.html':
+            case 'Noticias/MobileNews-5.html':
                 window.location.href = url;
                 break;
 
-            case 'Noticial/MobileNews-6.html':
+            case 'Noticias/MobileNews-6.html':
             window.location.href = url;
             break;
 
-            case 'Noticial/MobileNews-7.html':
+            case 'Noticias/MobileNews-7.html':
                 window.location.href = url;
                 break;
 
-            case 'Noticial/MobileNews-8.html':
+            case 'Noticias/MobileNews-8.html':
             window.location.href = url;
             break;
 
-            case 'Noticial/MobileNews-9.html':
+            case 'Noticias/MobileNews-9.html':
                 window.location.href = url;
                 break;
             
         }
     })
+});
+
+const icon = document.getElementById('icon');
+
+icon.addEventListener('click', ()=> {
+    window.alert('HOLA?');
 })
