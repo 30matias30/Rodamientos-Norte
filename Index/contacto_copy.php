14e05bdb2aac00fde1f0ae35a7b3e0e3176f5292
Matias Astorga <30matias30@gmail.com>
2023-09-15 15:52:40 -0300

    Contact Form - Bug Fix

--- Index/contacto_copy.php
+++ Index/contacto_copy.php
@@ -42,16 +42,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rodamientos Norte</title>
-    <link rel="shortcut icon" href="./ICO/Group 2.png">
+    <link rel="shortcut icon" href="/Rodamientos-Norte/ICO/Group 2.png">
 
     <!-- Style -->
-    <link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="./preloader.css">
-    <link rel="stylesheet" href="./sliderNews.css">
-    <link rel="stylesheet" href="./second_style.css">
-    <link rel="stylesheet" href="./catalog-style.css">
-    <link rel="stylesheet" href="./contacto-style.css">
-    <link rel="stylesheet" href="./productos-style.css">
+    <link rel="stylesheet" href="/Rodamientos-Norte/Style/style.css">
+    <link rel="stylesheet" href="/Rodamientos-Norte/Style/preloader.css">
+    <link rel="stylesheet" href="/Rodamientos-Norte/Style/sliderNews.css">
+    <link rel="stylesheet" href="/Rodamientos-Norte/Style/institucional-style.css">
+    <link rel="stylesheet" href="/Rodamientos-Norte/Style/catalog-style.css">
+    <link rel="stylesheet" href="/Rodamientos-Norte/Style/contacto-style.css">
+    <link rel="stylesheet" href="/Rodamientos-Norte/Style/productos-style.css">
+
+    <!-- Mobile Style -->
+    <link rel="stylesheet" href="/Rodamientos-Norte/Style/mobile-style.css">
 
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -59,6 +62,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     
 </head>
+
 <body>
 
     <div id="preloader"></div>
@@ -67,26 +71,142 @@
 
     <div id="header" class="header">
 
+      <div>
+        <div class="header-element green"></div>
+        <div class="header-element red"></div>
+      </div>
+
         <div id="icon" class="icon"></div>
+
+        <div>
+          <div class="header-element red"></div>
+          <div class="header-element green"></div>
+        </div>
   
     </div>
   
     <div id="optionsBar"> 
     
+      <div>
+        <div id="bar" class="option option-script-mobile" data-url="sideBar">INICIO</div>
+        <div id="ini" class="option option-script" data-url="/Rodamientos-Norte/index.html">INICIO</div>
+        <div id="prod" class="option option-script" data-url="/Rodamientos-Norte/Index/productos.html">PRODUCTOS</div>
+        <div id="inst" class="option option-script" data-url="/Rodamientos-Norte/Index/institucional.html">INSTITUCIONAL</div>
+        <div id="catalog" class="option option-script" data-url="/Rodamientos-Norte/Index/catalogos.html">CATALOGOS</div>
+        <div id="contact" class="option option-script" data-url="/Rodamientos-Norte/Index/contacto.html">CONTACTO</div>
+      </div>
+    </div>
+
+    <div id="sideBar" class="sideBar">
+
+      <div id="header" class="header">
+
         <div>
-          <div id="ini" class="option" data-url="./index.html">NORTE</div>
-          <div id="prod" class="option" data-url="./productos.html">PRODUCTOS</div>
-          <div id="inst" class="option" data-url="./institucional.html">INSTITUCIONAL</div>
-          <div id="catalog" class="option" data-url="./catalogos.html">CATALOGOS</div>
-          <div id="contact" class="option" data-url="./contacto.html">CONTACTO</div>
+          <div class="header-element green"></div>
+          <div class="header-element red"></div>
         </div>
+  
+          <div id="icon" class="icon"></div>
+  
+          <div>
+            <div class="header-element red"></div>
+            <div class="header-element green"></div>
+          </div>
+    
+      </div>
+
+      <div class="close-btn">
+        <span></span>
+        <span></span>
+      </div>
+
+      <div class="links">
+
+        <div id="ini" class="option option-script" data-url="/Rodamientos-Norte/index.html">NORTE</div>
+        <div id="prod" class="option option-script" data-url="/Rodamientos-Norte/Index/productos.html">PRODUCTOS</div>
+        <div id="inst" class="option option-script" data-url="/Rodamientos-Norte/Index/institucional.html">INSTITUCIONAL</div>
+        <div id="catalog" class="option option-script" data-url="/Rodamientos-Norte/Index/catalogos.html">CATALOGOS</div>
+        <div id="contact" class="option option-script" data-url="/Rodamientos-Norte/Index/contacto.html">CONTACTO</div>
+
+      </div>
+
+      <div class="sideBar-footer"></div>
 
     </div>
+
        <!-- NO CHANGES FROM THE INDEX - LANDING -->
 
+       <div class="contact-top-body mobile-contact-top-body">
+
+        <h1>ENCONTRANOS EN MAPS</h1>
+
+        <div class="contact-map-container">
+
+            <div class="jm-map map-card">
+                <h2>JESUS MARIA</h2>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d427.56414233631193!2d-64.0903262963276!3d-30.984066015970395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94326399cc04ffef%3A0x355a867a8a4c1a97!2sRodamientos%20NORTE!5e0!3m2!1ses!2sar!4v1691704067833!5m2!1ses!2sar" width="305
+                px" height="320" style="border-radius:10px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div class="direction">
+                  <p>Ameghino 496, esquina J.B. Alberdi - Jesús María - Córdoba</p>
+                </div>
+            </div>
+
+            <div class="vt-map map-card">
+                <h2>VILLA DEL TOTORAL</h2>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1212.8089934929094!2d-64.061013848688!3d-30.708788598308214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94326de0cbf60f59%3A0x448a962e2dea9bc4!2sTorner%C3%ADa%20Vendrame%20-%20Rodamientos%20Norte!5e0!3m2!1ses!2sar!4v1691704129299!5m2!1ses!2sar" width="305
+                " height="320" style="border-radius:10px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div class="direction">
+                  <p>Calle los Timones S/N - Villa del Totoral - Córdoba</p>
+                </div>
+            </div>
+
+        </div>
+
+        <h1>COMUNICATE CON NOSOTROS</h1>
+
+        <p>Comunicate con nosotros a traves de las lineas telefonicas y whatsapp. Nuestro personal esta listo para escucharte y asesorarte en sus dudas. </p>
+
+        <div class="contact-links">
+
+            <div class="contact-links-card">
+                <h1>JESUS MARIA </h1>
+                
+                <div>
+                  <img src="/Rodamientos-Norte/ICO/phone (verde).svg" alt="" srcset="">
+                  <div>(3525) 608668</div>
+                </div>
+                <div>
+                  <img src="/Rodamientos-Norte/ICO/WA(verde).svg" alt="" srcset="">
+                  <div>(3525) 478688 // 416731</div>
+                </div>         
+                
+            </div>
+
+            <div class="contact-links-card">
+                <h1>VILLA DEL TOTORAL</h1>
+
+                <div>
+                  <img src="/Rodamientos-Norte/ICO/phone (verde).svg" alt="" srcset="">
+                  <div>(3525) 608668</div>
+                </div>
+                <div>
+                  <img src="/Rodamientos-Norte/ICO/WA(verde).svg" alt="" srcset="">
+                  <div>(3525) 478688 // 416731</div>
+                </div>         
 
 
-    <div class="contact-top-body">
+            </div>
+
+        </div>
+
+        <h1>ESCRIBINOS Y TE RESPONDEREMOS</h1>
+
+    </div>
+
+
+
+
+    <div class="contact-top-body desktop-contact-top-body">
 
         <h1>ENCONTRANOS EN MAPS</h1>
 
@@ -120,11 +240,11 @@
                 <h1>JESUS MARIA </h1>
                 
                 <div>
-                  <img src="./ICO/phone (verde).svg" alt="" srcset="">
+                  <img src="/Rodamientos-Norte/ICO/phone (verde).svg" alt="" srcset="">
                   <div>(3525) 608668</div>
                 </div>
                 <div>
-                  <img src="./ICO/WA(verde).svg" alt="" srcset="">
+                  <img src="/Rodamientos-Norte/ICO/WA(verde).svg" alt="" srcset="">
                   <div>(3525) 478688 // 416731</div>
                 </div>         
                 
@@ -134,11 +254,11 @@
                 <h1>VILLA DEL TOTORAL</h1>
 
                 <div>
-                  <img src="./ICO/phone (verde).svg" alt="" srcset="">
+                  <img src="/Rodamientos-Norte/ICO/phone (verde).svg" alt="" srcset="">
                   <div>(3525) 608668</div>
                 </div>
                 <div>
-                  <img src="./ICO/WA(verde).svg" alt="" srcset="">
+                  <img src="/Rodamientos-Norte/ICO/WA(verde).svg" alt="" srcset="">
                   <div>(3525) 478688 // 416731</div>
                 </div>         
 
@@ -157,7 +277,7 @@
       
       <div></div>
 
-      <div class="contact-form-card">
+      <div class="contact-form-card desktop-contact-form">
 
         <p>Porfavor, complete los datos solicitados para una mejor comunicacion</p>
         <div></div>
@@ -165,22 +285,22 @@
         <form action="./" method="post">
           <div>
 
-            <input type="text" name="nombre" id="nombre" placeholder="Nombre y Apellido">
-            <input type="text" name="provincia" id="provincia" placeholder="Provincia">
+            <input type="text" name="" id="nombre" placeholder="Nombre y Apellido">
+            <input type="text" name="" id="provincia" placeholder="Provincia">
   
 
           </div>
           <div>
 
-            <input type="email" name="email" id="email" placeholder="Correo Electrónico">
-            <input type="text" name="localidad" id="localidad" placeholder="Localidad">
-            <input type="number" name="postal" min="0" max="4" id="cdp" placeholder="C. Postal">
+            <input type="email" name="" id="email" placeholder="Correo Electrónico">
+            <input type="text" name="" id="localidad" placeholder="Localidad">
+            <input type="number" min="0" max="4" id="cdp" placeholder="C. Postal">
 
           </div>
           <div>
 
-            <input type="tel" name="phone" id="phone" placeholder="Teléfono/Celular">
-            <textarea name="mensaje" id="mensaje" cols="30" rows="10" placeholder="Escribe tu mensaje..."></textarea>
+            <input type="tel" name="" id="phone" placeholder="Teléfono/Celular">
+            <textarea name="" id="mensaje" cols="30" rows="10" placeholder="Escribe tu mensaje..."></textarea>
 
           </div>
 
@@ -189,6 +309,42 @@
 
         </form>
 
+      </div>
+
+      <div class="contact-form-card mobile-contact-form">
+
+        <p>Porfavor, complete los datos solicitados para una mejor comunicacion</p>
+        <div></div>
+
+        <form action="./" method="post">
+          <div id="contact-form-card1">
+
+            <input type="text" name="" id="nombre" placeholder="Nombre y Apellido">
+            <input type="email" name="" id="email" placeholder="Correo Electrónico">
+            <input type="tel" name="" id="phone" placeholder="Teléfono/Celular">
+            <div class="nextButton nextButton1"><p>SIGUIENTE</p></div>  
+
+          </div>
+          <div id="contact-form-card2" class="contact-form-card-invisible">
+
+            <input type="text" name="" id="provincia" placeholder="Provincia">
+            <input type="text" name="" id="localidad" placeholder="Localidad">
+            <input type="number" min="0" max="4" id="cdp" placeholder="C. Postal">
+            <div class="nextButton nextButton2"><p>SIGUIENTE</p></div>  
+
+
+          </div>
+          <div id="contact-form-card-invisible" class="contact-form-card-invisible">
+
+            <textarea name="" id="mensaje" cols="30" rows="10" placeholder="Escribe tu mensaje..."></textarea>
+            <button id="contact-form-btn" type="submit">ENVIAR</button>
+
+          </div>
+
+          
+
+
+        </form>
 
       </div>
 
@@ -196,55 +352,105 @@
     
 
 
-       <!-- NO CHANGES FROM THE INDEX - LANDING -->
+    <!-- NO CHANGES FROM THE INDEX - LANDING -->
 
-       <div class="green-separator">
+    <div class="green-separator mobile-green-separator">
+  
+      <div><h1>SOMOS <br> RODAMIENTOS NORTE</h1></div>
+      <p>En Rodamientos Norte, nuestra pasión por el servicio es lo que impulsa cada acción que tomamos. Nos apasiona brindar soluciones efectivas y ser la opción en la que nuestros clientes confíen para sus proyectos.</p>
+      <img src="/Rodamientos-Norte/ICO/Vector.svg" alt="" srcset="">
 
-        <div><h1>SOMOS RODAMIENTOS NORTE</h1><img src="./ICO/Vector.svg" alt="" srcset=""></div>
-        <p>En Rodamientos Norte, nuestra pasión por el servicio es lo que impulsa cada acción que tomamos. Nos apasiona brindar soluciones efectivas y ser la opción en la que nuestros clientes confíen para sus proyectos.</p>
-  
-      </div>
-  
-      <div class="footer-photo">
-  
-        <img src="./Pictures/B&W-footer.png" alt="" srcset="">
-  
-      </div>
-  
-      <div class="footer">
-  
-        <div id="icon-footer"></div>
-        <div class="options-footer">
-  
-          <p>MENÚ</p>
-  
-          <a href="">INICIO</a>
-          <a href="">PRODUCTOS</a>
-          <a href="">CATÁLOGOS</a>
-          <a href="">INSTITUCIONAL</a>
-          <a href="">CONTACTO</a>
+  </div>
+
+
+    <div class="green-separator desktop-green-separator">
+
+      <div><h1>SOMOS RODAMIENTOS NORTE</h1><img src="/Rodamientos-Norte/ICO/Vector.svg" alt="" srcset=""></div>
+      <p>En Rodamientos Norte, nuestra pasión por el servicio es lo que impulsa cada acción que tomamos. Nos apasiona brindar soluciones efectivas y ser la opción en la que nuestros clientes confíen para sus proyectos.</p>
+
+    </div>
+
+    <div class="footer-photo">
+
+      <img src="/Rodamientos-Norte/Pictures/B&W-footer.png" alt="" srcset="">
+
+    </div>
+
+    <div class="footer mobile-footer">
+
+      <div class="icon-footer"></div>
+
+      <div class="contact-footer">
+
+          <p class="title">Villa del Totoral</p>
+          <p><span class="phone-footer contact-ico"><img src="/Rodamientos-Norte/ICO/phone - svg.svg" alt=""></span>3525-608668</p>
+          <p><span class="whatsApp-footer contact-ico"><img src="/Rodamientos-Norte/ICO/WA - svg.svg" alt=""></span>3525478691 // 3525416731</p>
+          <p><span class="pointer-footer contact-ico"><img src="/Rodamientos-Norte/ICO/pin - svg.svg" alt=""></span>Calle los Timones S/N - Villa del Totoral - Córdoba</p>
+
+          <p class="title">Jesús María</p>
+          <p><span class="phone-footer contact-ico"><img src="/Rodamientos-Norte/ICO/phone - svg.svg" alt=""></span>3525-608668</p>
+          <p><span class="whatsApp-footer contact-ico"><img src="/Rodamientos-Norte/ICO/WA - svg.svg" alt=""></span>3525478691 // 3525416731</p>
+          <p><span class="pointer-footer contact-ico"><img src="/Rodamientos-Norte/ICO/pin - svg.svg" alt=""></span>Ameghino 496, esquina J.B. Alberdi - Jesús María - Córdoba</p>
   
         </div>
-        <div class="contact-footer">
+
+        <div class="options-footer">
   
-          <p>DIRECCIÓN Y TELEFONOS</p>
+          <a href="/Rodamientos-Norte/index.html">INICIO</a>
+          <a href="/Rodamientos-Norte/Index/productos.html">PRODUCTOS</a>
+          <a href="/Rodamientos-Norte/Index/catalogos.html">CATÁLOGOS</a>
+          <a href="/Rodamientos-Norte/Index/institucional.html">INSTITUCIONAL</a>
+          <a href="/Rodamientos-Norte/Index/contacto.html">CONTACTO</a>
   
-          <div><div class="pointer-footer contact-ico"><img src="./ICO/pin - svg.svg" alt=""></div>Ameghino 496, esquina J.B. Alberdi - Jesús María - Córdoba</div>
-          <div>
-            <div><div class="whatsApp-footer contact-ico"><img src="./ICO/WA - svg.svg" alt=""></div>3525478691 // 3525416731</div>
-            <div><div class="phone-footer contact-ico"><img src="./ICO/phone - svg.svg" alt=""></div>3525-608668</div>
-          </div>
+        </div>
+
+      </div>
+
+
+    <div class="footer desktop-footer">
+
+      <div class="icon-footer"></div>
+      <div class="options-footer">
+
+        <p>MENÚ</p>
+
+        <a href="/Rodamientos-Norte/index.html">INICIO</a>
+        <a href="/Rodamientos-Norte/Index/productos.html">PRODUCTOS</a>
+        <a href="/Rodamientos-Norte/Index/catalogos.html">CATÁLOGOS</a>
+        <a href="/Rodamientos-Norte/Index/institucional.html">INSTITUCIONAL</a>
+        <a href="/Rodamientos-Norte/Index/contacto.html">CONTACTO</a>
+
+      </div>
+      <div class="contact-footer">
+
+        <p>DIRECCIÓN Y TELEFONOS</p>
+
+        <div><div class="pointer-footer contact-ico"><img src="/Rodamientos-Norte/ICO/pin - svg.svg" alt=""></div>Ameghino 496, esquina J.B. Alberdi - Jesús María - Córdoba</div>
+        <div>
+          <div><div class="whatsApp-footer contact-ico"><img src="/Rodamientos-Norte/ICO/WA - svg.svg" alt=""></div>3525478691 // 3525416731</div>
+          <div><div class="phone-footer contact-ico"><img src="/Rodamientos-Norte/ICO/phone - svg.svg" alt=""></div>3525-608668</div>
+        </div>
+
+        <div><div class="pointer-footer contact-ico"><img src="/Rodamientos-Norte/ICO/pin - svg.svg" alt=""></div>Calle los Timones S/N - Villa del Totoral - Córdoba</div>
+        <div>
+          <div><div class="whatsApp-footer contact-ico"><img src="/Rodamientos-Norte/ICO/WA - svg.svg" alt=""></div>3525478691 // 3525416731</div>
+          <div><div class="phone-footer contact-ico"><img src="/Rodamientos-Norte/ICO/phone - svg.svg" alt=""></div>3525-608668</div>
+        </div>
+
+      </div>  
+
   
-          <div><div class="pointer-footer contact-ico"><img src="./ICO/pin - svg.svg" alt=""></div>Calle los Timones S/N - Villa del Totoral - Córdoba</div>
-          <div>
-            <div><div class="whatsApp-footer contact-ico"><img src="./ICO/WA - svg.svg" alt=""></div>3525478691 // 3525416731</div>
-            <div><div class="phone-footer contact-ico"><img src="./ICO/phone - svg.svg" alt=""></div>3525-608668</div>
-          </div>
-  
-        </div>  
-  
-    
-      <script src="./scrypt.js"></script>
+    <script src="/Rodamientos-Norte/Script/scrypt.js"></script>
+    <script src="/Rodamientos-Norte/Script/ContactForm-script.js"></script>
+    <script>
+
+      let scriptMobileBtn = document.querySelector('.option-script-mobile');
+
+      scriptMobileBtn.addEventListener('click', function(){
+          sideBar.classList.toggle('actived');
+      })
+
+    </script>
 
 </body>
 </html>
